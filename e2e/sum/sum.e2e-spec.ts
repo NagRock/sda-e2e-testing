7b4ed69ec6c2ff7c7444309bc0a58d8b2761ec06
mkuster <kuster.maciej@gmail.com>
2017-08-25 17:41:50 +0200

    sum e2e template

---
+++ e2e/sum/sum.e2e-spec.ts
@@ -0,0 +1,11 @@
+import {browser} from 'protractor';
+
+describe('sum', () => {
+  beforeEach(() => {
+    browser.get('/');
+  });
+
+  it('should sum two numbers', async (done) => {
+    done();
+  });
+});
